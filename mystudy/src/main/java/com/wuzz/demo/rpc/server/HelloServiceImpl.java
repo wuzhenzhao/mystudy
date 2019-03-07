6ba3ff199eeb036e05f97f4478888193e12f1eec
wuzhenzhao <774283325@qq.com>
2019-03-07 17:11:27 +0800

    1

--- mystudy/src/main/java/com/wuzz/demo/rpc/server/HelloServiceImpl.java
+++ mystudy/src/main/java/com/wuzz/demo/rpc/server/HelloServiceImpl.java
@@ -3,6 +3,6 @@ package com.wuzz.demo.rpc.server;
 public class HelloServiceImpl implements HelloService {
 
     public String sayHello(String msg) {
-        return msg+"hello world";
+        return msg+" Hello world";
     }
 }
