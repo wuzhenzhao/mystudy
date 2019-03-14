976925702d3c94bbacaaf19047b1470c4f0e4faf
wuzhenzhao <774283325@qq.com>
2019-03-14 09:17:10 +0800

    日志

---
+++ mystudy/netty-study/src/main/java/com/wuzz/demo/netty/NettyApp.java
@@ -0,0 +1,24 @@
+package com.wuzz.demo.netty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create with IntelliJ IDEA
+ * User: Wuzhenzhao
+ * Date: 2019/3/13
+ * Time: 18:13
+ * Description:
+ */
+@SpringBootApplication
+public class NettyApp {
+    private final static Logger log = LoggerFactory.getLogger(NettyApp.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyApp.class,args);
+        log.info("服务启动成功");
+
+    }
+}
