dd0316e961440f517b6942e971387305513dcae0
wuzhenzhao <774283325@qq.com>
2019-08-16 09:51:53 +0800

    111

--- mystudy/dubbo-study-2.7.2/dubbo-client-2.7.2/src/main/java/com/wuzz/demo/App.java
+++ mystudy/dubbo-study-2.7.2/dubbo-client-2.7.2/src/main/java/com/wuzz/demo/App.java
@@ -20,10 +20,10 @@ public class App {
         ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext("META-INF/spring/dubbo-client.xml");
         for(int i=0;i<11;i++) {
             // 得到IGpHello的远程代理对象
-//            HelloService helloService = (HelloService) context1.getBean("helloService");
-            UserService userService = (UserService) context1.getBean("userService");
-//            System.out.println(helloService.sayHello("WUZZ"));
-            System.out.println(userService.register(i));
+            HelloService helloService = (HelloService) context1.getBean("helloService");
+//            UserService userService = (UserService) context1.getBean("userService");
+            System.out.println(helloService.sayHello("WUZZ"));
+//            System.out.println(userService.register(i));
             Thread.sleep(1000);
         }
         System.in.read();
