5ed464f5c97091e551b86f2c4567c06e4b11e41b
wuzhenzhao <774283325@qq.com>
2020-01-07 15:24:38 +0800

    activemq 消息延迟及定时发送

--- mystudy/activemq-study/src/main/java/com/wuzz/demo/demo/Provider.java
+++ mystudy/activemq-study/src/main/java/com/wuzz/demo/demo/Provider.java
@@ -2,6 +2,7 @@ package com.wuzz.demo.demo;
 
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ScheduledMessage;
 
 import javax.jms.*;
 
@@ -30,6 +31,24 @@ public class Provider {
             // 创建消费者
             MessageProducer producer = session.createProducer(destination);
             TextMessage message = session.createTextMessage("Hello World");
+            //延迟60秒发送消息
+//            long time = 60 * 1000;
+//            message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, time);
+            //开始延迟30秒发送，重复发送10次，每次之间间隔10秒
+//            long delay = 30 * 1000;
+//            long period = 10 * 1000;
+//            int repeat = 9;
+//            message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, delay);
+//            message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_PERIOD, period);
+//            message.setIntProperty(ScheduledMessage.AMQ_SCHEDULED_REPEAT, repeat);
+            //使用Cron 表示式定时发送消息
+//            message.setStringProperty(ScheduledMessage.AMQ_SCHEDULED_CRON, "0 * * * *");
+            //Cron 的优先级大于消息延迟，只要设置了Cron 表达式会优先执行Cron规则，如下：消息定时发送10次，每个小时执行，延迟1秒之后发送。
+
+//            message.setStringProperty(ScheduledMessage.AMQ_SCHEDULED_CRON, "0 * * * *");
+//            message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, 1000);
+//            message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_PERIOD, 1000);
+//            message.setIntProperty(ScheduledMessage.AMQ_SCHEDULED_REPEAT, 9);
             producer.send(message);
             // 表示消息被自动确认
             session.commit();
