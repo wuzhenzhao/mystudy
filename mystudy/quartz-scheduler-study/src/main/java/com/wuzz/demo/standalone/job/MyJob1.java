6f29b2bc8a532871efa6b9e9960ea3a62aa51d58
wuzhenzhao <774283325@qq.com>
2020-01-08 16:12:58 +0800

    禁止任务并发执行

--- mystudy/quartz-scheduler-study/src/main/java/com/wuzz/demo/standalone/job/MyJob1.java
+++ mystudy/quartz-scheduler-study/src/main/java/com/wuzz/demo/standalone/job/MyJob1.java
@@ -1,9 +1,6 @@
 package com.wuzz.demo.standalone.job;
 
-import org.quartz.Job;
-import org.quartz.JobDataMap;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,12 +12,20 @@ import java.util.Date;
  * Time: 20:12
  * Description 描述:
  */
+//防止一个jobdetail并发执行任务
+//@DisallowConcurrentExecution
 public class MyJob1 implements Job {
 
     public void execute(JobExecutionContext context) throws JobExecutionException {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Date date = new Date();
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
-        System.out.println( " " + sf.format(date) + " 任务1执行了，"+dataMap.get("name"));
+        System.out.println( " " + sf.format(date) + " 任务1执行了，"+dataMap.get("name")+"=========="+context.getJobDetail().isConcurrentExectionDisallowed());
     }
 }
