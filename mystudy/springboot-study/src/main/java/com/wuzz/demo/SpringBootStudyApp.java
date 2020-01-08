3c6fef6d2c6c3b64d56628ef84f111861064531f
wuzhenzhao <774283325@qq.com>
2020-01-08 16:27:07 +0800

    springboot定时任务demo

--- mystudy/springboot-study/src/main/java/com/wuzz/demo/SpringBootStudyApp.java
+++ mystudy/springboot-study/src/main/java/com/wuzz/demo/SpringBootStudyApp.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @EnableSwagger2
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootStudyApp {
     private final static Logger log = LoggerFactory.getLogger(SpringBootStudyApp.class);
 
---
+++ mystudy/springboot-study/src/main/java/com/wuzz/demo/task/TaskUtil.java
@@ -0,0 +1,20 @@
+package com.wuzz.demo.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * User: Wuzhenzhao
+ * Date: 2020/1/8
+ * Time: 16:18
+ * Description:
+ * ClassPath:com.wuzz.demo.task.TaskUtil
+ */
+@Component
+public class TaskUtil {
+
+    @Scheduled(cron = "0/2 * * * * ?")
+    private void task1(){
+        System.out.println("任务执行");
+    }
+}
