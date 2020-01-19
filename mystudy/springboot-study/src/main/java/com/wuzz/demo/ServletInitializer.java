4d44cade2c38b75762d9796dc68cdac8439dd0cf
wuzhenzhao <774283325@qq.com>
2020-01-19 09:43:21 +0800

    无关的修改

---
+++ mystudy/springboot-study/src/main/java/com/wuzz/demo/ServletInitializer.java
@@ -0,0 +1,22 @@
+//package com.wuzz.demo;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+///**
+// * User: Wuzhenzhao
+// * Date: 2020/1/16
+// * Time: 12:23
+// * Description: 打成war包展开
+// * ClassPath:com.wuzz.demo.ServletInitializer
+// */
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        //此处的Application.class为带有@SpringBootApplication注解的启动类
+//        return builder.sources(SpringBootStudyApp.class);
+//    }
+//
+//}
+//
