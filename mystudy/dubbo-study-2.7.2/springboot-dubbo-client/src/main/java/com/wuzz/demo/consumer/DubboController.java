ff0b0cfb9d5e6d0307d9798f13979e14df501a10
wuzhenzhao <774283325@qq.com>
2020-04-24 11:08:34 +0800

    修改注释

--- mystudy/dubbo-study-2.7.2/springboot-dubbo-client/src/main/java/com/wuzz/demo/consumer/DubboController.java
+++ mystudy/dubbo-study-2.7.2/springboot-dubbo-client/src/main/java/com/wuzz/demo/consumer/DubboController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 腾讯课堂搜索【咕泡学院】
- * 官网：www.gupaoedu.com
- * 风骚的Mic 老师
  * create-date: 2019/7/21-20:42
  */
 @RestController
--- mystudy/dubbo-study-2.7.2/springboot-dubbo-client/src/main/java/com/wuzz/demo/mock/HelloServiceMock.java
+++ mystudy/dubbo-study-2.7.2/springboot-dubbo-client/src/main/java/com/wuzz/demo/mock/HelloServiceMock.java
@@ -3,9 +3,6 @@ package com.wuzz.demo.mock;
 import com.wuzz.demo.api.HelloService;
 
 /**
- * 腾讯课堂搜索【咕泡学院】
- * 官网：www.gupaoedu.com
- * 风骚的Mic 老师
  * create-date: 2019/7/21-21:28
  * 降级处理的类
  */
