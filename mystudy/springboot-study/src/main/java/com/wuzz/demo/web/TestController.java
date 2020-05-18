09b9e4416ed1014470c410d1466c9b3ee0a20152
wuzhenzhao <774283325@qq.com>
2020-05-18 20:33:19 +0800

    日志切换

--- mystudy/springboot-study/src/main/java/com/wuzz/demo/web/TestController.java
+++ mystudy/springboot-study/src/main/java/com/wuzz/demo/web/TestController.java
@@ -1,9 +1,12 @@
 package com.wuzz.demo.web;
 
+import com.wuzz.demo.SpringBootStudyApp;
 import com.wuzz.demo.core.Result;
 import com.wuzz.demo.core.exception.BusinessException;
 import com.wuzz.demo.core.exception.CommonErrorEnum;
 import com.wuzz.demo.entity.EntityDemo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("wuzz")
 public class TestController {
-
+    private final static Logger log = LoggerFactory.getLogger(TestController.class);
     @RequestMapping(value = "/post.json", method = {RequestMethod.POST})
     public Result insert(EntityDemo entity) {
-
+        log.info("进入insert方法。。。。。。。");
         entity.validate();
-        if (entity.getId() == null)
+        if (entity.getId() == null) {
+            log.info("ID 为空。。。。");
             throw new BusinessException(CommonErrorEnum.PARAMER_ERROR);
+        }
 //            throw new BusinessException(new ErrorObject("001", "error", "数据异常"));
 
         return new Result(true, "000", "成功: " + entity);
@@ -32,14 +37,17 @@ public class TestController {
 
     @RequestMapping(value = "/get.json", method = {RequestMethod.GET})
     public Result get(String name) {
-
-        if (name == null)
+        log.info("进入get方法。。。。。");
+        if (name == null) {
+            log.info("name为空。。。。。。。");
             throw new BusinessException(CommonErrorEnum.PARAMER_ERROR);
+        }
         return new Result(true, "000", "成功: ");
     }
 
     @RequestMapping(value = "pus", method = {RequestMethod.GET})
     public Result get1(EntityDemo entity) {
+        log.info("进入pus方法。。。。。");
         entity.validate();
         return new Result(true, "000", "成功1111111: ");
     }
