169fa581b13afd1860790e6569360d951432b3f7
wuzhenzhao <774283325@qq.com>
2020-07-06 20:22:48 +0800

    spring cloud oauth2

--- mystudy/spring-cloud-study/spring-cloud-oauth2/oauth-authorization-server/src/main/java/com/wuzz/demo/oauth/config/WebSecurityConfig.java
+++ mystudy/spring-cloud-study/spring-cloud-oauth2/oauth-authorization-server/src/main/java/com/wuzz/demo/oauth/config/WebSecurityConfig.java
@@ -1,20 +1,10 @@
 package com.wuzz.demo.oauth.config;
 
-import com.wuzz.demo.oauth.MyAuthenctiationFailureHandler;
-import com.wuzz.demo.oauth.MyAuthenticationSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @description: 类功能描述
@@ -26,19 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity// 开启Security
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // 自定义  token 分发类型
-    @Autowired
-    private MyAuthenticationSuccessHandler myAuthenticationSuccessHandler;
-
-    @Autowired
-    private MyAuthenctiationFailureHandler myAuthenctiationFailureHandler;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
 
     @Bean
     @Override
@@ -46,30 +23,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManager();
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-//        http
-//                .httpBasic().and()
-//                .formLogin().permitAll()
-//                .loginPage("http://localhost:8081/#/login")//自定义登陆地址
-//                .loginProcessingUrl("/authentication/form") //登录处理地址
-//                .successHandler(myAuthenticationSuccessHandler) // 登陆成功处理器
-//                .failureHandler(myAuthenctiationFailureHandler) // 登陆失败处理器
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/authentication/form", "/login").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .csrf().disable();
-
-        http    .userDetailsService(userDetailsService)
-                // 配置登陆页/login并允许访问
-                .formLogin().permitAll()
-                // 登出页
-                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
-                // 其余所有请求全部需要鉴权认证
-                .and().authorizeRequests().anyRequest().authenticated()
-                // 由于使用的是JWT，我们这里不需要csrf
-                .and().csrf().disable();
-    }
 }
--- mystudy/spring-cloud-study/spring-cloud-oauth2/oauth-authorization-server/src/main/java/com/wuzz/demo/oauth/config/WuzzResourceServerConfig.java
+++ mystudy/spring-cloud-study/spring-cloud-oauth2/oauth-authorization-server/src/main/java/com/wuzz/demo/oauth/config/WuzzResourceServerConfig.java
@@ -1,7 +1,12 @@
 package com.wuzz.demo.oauth.config;
 
+import com.wuzz.demo.oauth.MyAuthenctiationFailureHandler;
+import com.wuzz.demo.oauth.MyAuthenticationSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -15,8 +20,27 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class WuzzResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    // 自定义  token 分发类型
+    @Autowired
+    private MyAuthenticationSuccessHandler myAuthenticationSuccessHandler;
+
+    @Autowired
+    private MyAuthenctiationFailureHandler myAuthenctiationFailureHandler;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+                http
+                .httpBasic().and()
+                .formLogin().permitAll()
+                .loginPage("http://localhost:8081/#/login")//自定义登陆地址
+                .loginProcessingUrl("/authentication/form") //登录处理地址
+                .successHandler(myAuthenticationSuccessHandler) // 登陆成功处理器
+                .failureHandler(myAuthenctiationFailureHandler) // 登陆失败处理器
+                .and()
+                .authorizeRequests()
+                .antMatchers("/authentication/form", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
     }
 }
