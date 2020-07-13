3a27aee24266c9ca62a3159ac4a1a5f809807147
wuzhenzhao <774283325@qq.com>
2020-07-13 14:58:52 +0800

    二维码生成

--- mystudy/springboot-study/src/main/java/com/wuzz/demo/qrcode/RecodeUtil.java
+++ mystudy/springboot-study/src/main/java/com/wuzz/demo/qrcode/RecodeUtil.java
@@ -20,8 +20,6 @@ import static org.apache.catalina.manager.Constants.CHARSET;
 
 public class RecodeUtil {
 
-    private static String charset="utf-8";//定义字符编码
-    private static String QrName="jpg";//定义类型
     private static final int WIDTH = 30;
     private static final int HEIGHT = 30;
 
@@ -30,31 +28,22 @@ public class RecodeUtil {
         Hashtable hints = new Hashtable();
 
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H); //容错级别最高
-        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");  //设置字符编码
-        hints.put(EncodeHintType.MARGIN, 1);                //二维码空白区域,最小为0也有白边,只是很小,最小是6像素左右
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8"); //设置字符编码
+        hints.put(EncodeHintType.MARGIN, 1); //二维码空白区域,最小为0也有白边,只是很小,最小是6像素左右
         try {
             BitMatrix bitMatrix = new MultiFormatWriter().encode(contents, BarcodeFormat.QR_CODE, width, height, hints); // 1、读取文件转换为字节数组
-//            ByteArrayOutputStream out = new ByteArrayOutputStream();
             BufferedImage image = toBufferedImage(bitMatrix);
             //转换成png格式的IO流
             ImageIO.write(image, "png", response.getOutputStream());
-//            byte[] bytes = out.toByteArray();
-//            // 2、将字节数组转为二进制
-//            BASE64Encoder encoder = new BASE64Encoder();
-//            binary = encoder.encodeBuffer(bytes).trim();
-        } catch (WriterException e) { // TODO Auto-generated catch block
+        } catch (WriterException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-
     /**
      * image流数据处理
-     *
-     * @author ianly
      */
     public static BufferedImage toBufferedImage(BitMatrix matrix) throws Exception {
         int width = matrix.getWidth();
